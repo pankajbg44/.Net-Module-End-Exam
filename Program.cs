f9d5073c4bc381344994d3aee62d61d10cd7fee2
PankajBG <pankajbg44@gmail.com>
2023-07-16 12:30:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        double averageLength = CalculateAverageWordLength(input);
+
+        Console.WriteLine("Average word length: {0}", Math.Round(averageLength, 3));
+    }
+
+    static double CalculateAverageWordLength(string input)
+    {
+        string[] words = input.Split(' ');
+        int totalLength = 0;
+
+        foreach (string word in words)
+        {
+            totalLength += word.Length;
+        }
+
+        return (double)totalLength / words.Length;
+    }
+}
